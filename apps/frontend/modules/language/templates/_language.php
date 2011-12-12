dfffe31cea53e45e21bd15a7196faa7bf095fcd2
Emerson Estrella <emerson.estrella@gmail.com>
2011-12-12 02:32:28 -0200

    1

--- apps/frontend/modules/language/templates/_language.php
+++ apps/frontend/modules/language/templates/_language.php
@@ -1,4 +1,4 @@
-<form action="<?php echo url_for('change_language') ?>" id="langform">
+<form action="<?php echo url_for('change_language') ?>" id="langform" method="post">
   <?php echo $form['language']->render() ?>
   <?php echo $form->renderHiddenFields() ?>
   <input type="submit" value="ok" />
