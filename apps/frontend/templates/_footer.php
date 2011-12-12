fd1009347e609fd704d699307b2a86a2bba41d29
Emerson Estrella <emerson.estrella@gmail.com>
2011-12-12 01:14:26 -0200

    1

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -1,6 +1,6 @@
   <div data-role="footer" class="footer-docs" data-theme="c"> 
       <p>&copy; Futebol Clube Project</p>
-      <div id="lang" style="float: right;">
+      <div id="lang">
         <?php include_component('language', 'language') ?>
       </div>
   </div>  
