b57cfbf93b6b0b211b483952eb9c7b275922c040
Emerson Estrella <emerson.estrella@gmail.com>
2011-12-18 00:07:26 -0200

    1

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -14,3 +14,4 @@ require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
 sfContext::createInstance($configuration)->dispatch();
+
